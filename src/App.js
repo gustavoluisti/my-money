42522bd98425f9ba8a70eba17e89f883126ce95b
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2019-09-29 22:07:01 -0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import useGet from './useGet'
+
+const url = 'https://meusentimento-2c1f7.firebaseio.com/movimentacoes/2019-08.json'
+
+function App() {
+  const data = useGet(url)
+  const data2 = useGet('http://httpbin.org/ip')
+  return (
+    <div >
+      <h1>My Money</h1>
+      {JSON.stringify(data)}
+      {data.loading && <p>Loading...</p>}
+      <pre>{JSON.stringify(data2)}</pre>
+    </div>
+  );
+}
+
+export default App;
